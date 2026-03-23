c711b45d50ff0d9cb27e37ccce14e2b83510e8c8
Kuldeep Singh <ksmalik326@gmail.com>
2026-03-23 18:11:15 +0530

    Update base path and remote for root domain iamyamraj.github.io

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kuldeep_portfolio_live_website/",
+  base: "/",
 });
